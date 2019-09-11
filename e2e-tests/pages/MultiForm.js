37b37cdb5bf3e20029bd47c8a2fb22452df7694a
Thuvvareka <thuvvashan20@gmail.com>
2019-09-11 16:20:46 +0530

    Added few new methods

--- e2e-tests/pages/MultiForm.js
+++ e2e-tests/pages/MultiForm.js
@@ -3,6 +3,8 @@ var MultiForm = function () {
     var name = element(by.model('formData.name'));
     var email = element(by.model('formData.email'));
     var result1 = element(by.css('.ng-binding'));
+    var button=element(by.css('.btn'));
+    var checkBox=element.all(by.model('formData.type'));
 
     this.getHomePage = function (url) {
         browser.get(url);
@@ -16,9 +18,24 @@ var MultiForm = function () {
     this.getResults1 = function () {
         return result1;
     };
+    this.clickNextButton=function () {
+        return button;
+    };
+    this.clickCheckBox=function () {
+        return checkBox;
+    };
     this.profilePage = function (name, emailID) {
         this.setNameField().sendKeys(name);
         this.setEmail().sendKeys(emailID);
+        this.clickNextButton().click();
+    };
+    this.interestPage=function () {
+        this.clickCheckBox().get(1).click();
+        this.clickNextButton().click();
+    };
+    this.paymentPage=function () {
+        this.clickNextButton().click();
+
     }
 };
 module.exports = new MultiForm();
--- e2e-tests/tests/RunTest.js
+++ e2e-tests/tests/RunTest.js
@@ -13,5 +13,19 @@ describe('tests on multiform', function () {
         multiForm.getResults1().getText().then(function (value) {
             expect(value).toString('{"name":"devops","email":"gayasharan4@gmail.com"}')
         });
-    })
+    });
+
+    it('should select the second check box', function () {
+        multiForm.interestPage();
+        multiForm.getResults1().getText().then(function (value) {
+            expect(value).toString('{"name":"devops","email":"gayasharan4@gmail.com","type":"ps"}')
+        });
+        multiForm.paymentPage();
+    });
+
+    it('should submit the form', function () {
+        multiForm.paymentPage();
+    });
+
+
 });
